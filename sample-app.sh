1133e98d0d93e44ca569c9b560f2d4b11858ddc7
IsmailKarakayaa <47555011+IsmailKarakayaa@users.noreply.github.com>
2021-10-24 22:24:21 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 docker image build --tag testnetapplication .
-docker run -t -d -p 5050:5050 --name test testapp testnetapplication
+docker run -t -d -p 5050:5050 --name testapp testnetapplication
 docker ps -a
