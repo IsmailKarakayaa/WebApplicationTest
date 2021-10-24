bd4910cd3304c8891a5e6a9dbc4f3ba8a3eff9da
IsmailKarakayaa <47555011+IsmailKarakayaa@users.noreply.github.com>
2021-10-24 22:39:59 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-rm -rf tempdir
-mkdir tempdir
-cp -r WebApplicationTest/* tempdir/.
-
-cd tempdir || exit
 docker image build --tag testnetapplication .
 docker run -t -d -p 5050:5050 --name testapp testnetapplication
 docker ps -a
